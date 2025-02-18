cc442a76e9fe6db14c5954e4c940495a70b2ba24
Cliff Njoroge <cnjoroge925@gmail.com>
2025-02-18 21:12:21 +0300

    create a turbo repo ⚡

---
+++ packages/ui/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { ChatMessageList, ChatBubble, ChatBubbleAvatar, ChatBubbleMessage } from '$lib/index.js';
+</script>
+
+<div class="mx-auto grid max-w-xl gap-2 py-5">
+	// Wrap with ChatMessageList
+	<ChatMessageList>
+		// You can map over messages here
+		<ChatBubble variant="sent">
+			<ChatBubbleAvatar fallback="US" />
+			<ChatBubbleMessage variant="sent">
+				Hello, how has your day been? I hope you are doing well.
+			</ChatBubbleMessage>
+		</ChatBubble>
+
+		<ChatBubble variant="received">
+			<ChatBubbleAvatar fallback="AI" />
+			<ChatBubbleMessage variant="received">
+				Hi, I am doing well, thank you for asking. How can I help you today?
+			</ChatBubbleMessage>
+		</ChatBubble>
+
+		<ChatBubble variant="received">
+			<ChatBubbleAvatar fallback="AI" />
+			<ChatBubbleMessage isLoading />
+		</ChatBubble>
+	</ChatMessageList>
+</div>
